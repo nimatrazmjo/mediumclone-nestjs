f09b9e4599b6b3921cf23ddb7831a716370574bf
Nimatullah Razmjo <nimatullah.razmjo@gmail.com>
2023-03-22 18:00:16 +0500

    Add typeorm package and connect to postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import ORMConfig from './configs/orm.config';
 import { TagsModule } from './tags/tags.module';
 
 @Module({
-  imports: [TagsModule],
+  imports: [ TypeOrmModule.forRoot(ORMConfig), TagsModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/configs/orm.config.ts
@@ -0,0 +1,14 @@
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
+
+const ORMConfig: PostgresConnectionOptions = {
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'mediumclone',
+    password: 'root',
+    database: 'mediumclone',
+    entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+    synchronize: true,
+};
+
+export default ORMConfig;
